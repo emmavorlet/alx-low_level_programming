f2b6b8c383fc2cb8914dfd372a8f97886201f9a0
emmavorlet <emmavorlet@gmail.com>
2022-09-08 17:54:23 -0700

    print text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+
+
+/**
+ * main : funtion need to return integar at the end of execution
+ *
+ * return : return success and 1 as fail
+ */
+
+
+
+int main (void)
+{
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
+
+
+}	
