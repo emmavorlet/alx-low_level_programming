dc1e082e7ad8146ac13e5a1efc4b640c3b929184
emmavorlet <emmavorlet@gmail.com>
2022-09-08 18:22:54 -0700

    foitfwliygs

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - function need to return an integar after execution
+ *
+ * Return: return 0 as sucess and 1 as fail
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
