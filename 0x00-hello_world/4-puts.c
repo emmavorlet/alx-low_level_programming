832102bfceff20ab198941cfe3dab995368137c2
emmavorlet <emmavorlet@gmail.com>
2022-09-08 18:06:32 -0700

    adding text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 
-
-
-
 /**
- * main : funtion need to return integar at the end of execution
+ * main -  funtion need to return integar at the end of execution
  *
- * return : return success and 1 as fail
+ *Return: return success and 1 as fail
  */
 
-
-
-int main (void)
+int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
 
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
-}	
+}
