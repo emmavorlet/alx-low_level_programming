41348848be27a5db86b40a5f0b2e2e06fb678620
emmavorlet <emmavorlet@gmail.com>
2022-09-19 15:36:43 -0700

    replacing the value in an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+/**
+ * reset_to_98 - this function make the pointer take the input of n
+ * @n: pointer with the adress value
+ * Return: new value of n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
