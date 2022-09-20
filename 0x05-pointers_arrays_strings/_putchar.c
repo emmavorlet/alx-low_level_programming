2005e214fd5b07d996d2a497dbd6c2e3b688f631
emmavorlet <emmavorlet@gmail.com>
2022-09-19 17:49:37 -0700

    displaying a string

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+
+
+/**
+ *
+ *  * _puutchar - writes the character to stdout
+ *
+ *   * @c: the character to print
+ *
+ *    * Return: 0 as success
+ *
+ *     */
+
+
+
+int _putchar(char c)
+
+{
+
+
+
+		return (write(1, &c, 1));
+
+
+
+}
