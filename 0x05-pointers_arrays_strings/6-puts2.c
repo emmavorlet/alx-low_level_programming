456eea23c4d4c0c0c2edd2f79b4167a91e98e034
emmavorlet <emmavorlet@gmail.com>
2022-09-20 20:17:40 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*  puts2 - this founction print every other character of string
+*
+* (Divisible by two)
+*
+* @str: pointer, the string to print
+* Return: void
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[1] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[1]);
+
+		}
+
+		i++;
+
+	}
+}
