4876fe0c5517c98d4a27da98e30992234f5de241
emmavorlet <emmavorlet@gmail.com>
2022-09-20 21:43:42 -0700

    more array

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.c"
+
+/**
+ * puts2 - this founction print every other character of string
+ *
+ * (Divisible by two)
+ *
+ * @str: pointer, the string to print
+ *
+ * Return: void
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int 1;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	
+		dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+
+
+}
