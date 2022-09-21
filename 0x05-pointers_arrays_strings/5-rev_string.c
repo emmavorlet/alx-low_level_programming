0f3e40763d537517e0af21b7f88a0d833b5b902e
emmavorlet <emmavorlet@gmail.com>
2022-09-20 19:56:34 -0700

    reversing a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* rev_string - prints reversed string, followed by a new line
+*
+* @s: pointer to the string to print
+*
+* Return: void
+*
+*/
+
+void rev_string(char *s)
+{
+	int f = 0;
+	int b = 0;
+	int z = 0;
+
+	while (s[b])
+	{
+		b++;
+	}
+
+	b = b - 1;
+
+	while (f < b + 1 / 2)
+	{
+		z = s[f];
+		s[f] = s[b];
+		s[b] = z;
+
+		b++;
+		f++;
+	}
+}
+
